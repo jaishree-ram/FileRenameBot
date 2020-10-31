f597b030a102ce45391c03001235b639e830874a
Jai Shree ram <73603048+jaishree-ram@users.noreply.github.com>
2020-10-31 23:16:33 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -67,10 +67,6 @@ async def start(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.START_TEXT.format(update.from_user.first_name),
-        reply_markup=InlineKeyboardMarkup(
-        ),
-        reply_to_message_id=update.message_id
-    )
 
 @pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
 async def upgrade(bot, update):
